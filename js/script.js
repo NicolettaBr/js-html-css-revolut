76f8dce6bf9180c3f218cf74ae11e2ea9ebbe0fc
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-04-09 18:12:41 +0200

    js completo

--- js/script.js
+++ js/script.js
@@ -1,17 +1,27 @@
 $(document).ready(function(){
 
     //quando il mouse passa sopra agli li 
-        //individuo l' elemento cliccato con this
+        //individuo l' elemento con this
         //sull' elemento trovato individuo il figlio con la classe dropdown
         //aggiungo la classe active per renderlo visibile
 
     $('.menu li').mouseenter(function(){
         //console.log(this);
         var thisElement = $(this);
-        var figlio = thisElement.children('dropdown');
-        console.log(figlio);
+        var add = thisElement.children('.dropdown').addClass('active');
+        //console.log(add);
         
     });
+
+    //quando il mouse esce dagli li 
+        //individuo l' elemento con this
+        //sull' elemento trovato individuo il figlio con la classe dropdown
+        //tolgo la classe active per renderlo non visibile
+
+    $('.menu li').mouseleave(function(){
+        var remove = $(this).children('.dropdown').removeClass('active');
+        console.log(remove);
+    })
 
 
 
