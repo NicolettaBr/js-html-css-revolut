d0320dbd1332239392d582b6320820f83feaebe3
Nicoletta Berrettoni <nico.berrettoni@gmail.com>
2021-04-09 17:10:56 +0200

    inizio javascript

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
 
+    //quando il mouse passa sopra agli li 
+        //individuo l' elemento cliccato con this
+        //sull' elemento trovato individuo il figlio con la classe dropdown
+        //aggiungo la classe active per renderlo visibile
 
-
-
-
-
-
-
-
-
-
+    $('.menu li').mouseenter(function(){
+        //console.log(this);
+        var thisElement = $(this);
+        thisElement.children('dropdown');
+        console.log(thisElement);
+    });
 
 
 
